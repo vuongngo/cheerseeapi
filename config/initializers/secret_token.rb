b4203d96303e20860db2095d944791f2d5f22797
vuongngo <vuongngo.pd@gmail.com>
2015-01-22 18:09:36 +0700

    Initializes the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CheerseeApi::Application.config.secret_key_base = '5318197a09a060234e6733cfc9b60a20a1d1f22f37926b862dc6c0ab8dffb975e21dd10e9e3f2bd4593ea0013c8e020df30c60c78b74446c41f53cea2e420733'
