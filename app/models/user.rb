a6363052ab0e66333b4c850fdccd14c6313fe417
vuongngo <vuongngo.pd@gmail.com>
2015-01-24 10:39:25 +0700

    Add auth_token to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,17 @@ class User
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+  field :auth_token,        type: String, default: ""
+  validates :auth_token, uniqueness: true
+  index({ auth_token: 1 }, { unique: true })
+
+  before_create :generate_authentication_token!
+
+  protected
+    def generate_authentication_token!
+      self.auth_token = loop do
+        random_token = SecureRandom.urlsafe_base64(nil, false)
+        break random_token unless User.all.exists?(auth_token: random_token)
+      end
+    end
 end
