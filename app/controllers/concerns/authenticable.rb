6b8d450238a90e392dfe69045b1e2e4a7a8b7b51
vuongngo <vuongngo.pd@gmail.com>
2015-01-24 11:56:44 +0700

    Adds authenticalbe module for managing authenticable methods

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,11 @@
+module Authenticable
+
+  #Devise methods overwrite
+  def current_user
+  	@current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+  end
+
+  def request
+    request
+  end
+end
